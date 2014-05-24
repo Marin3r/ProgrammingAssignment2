c198fbc6682ba7a8776f795d0d808de580241e69
Vasco <vascodegouveia@yahoo.co.uk>
2014-05-24 17:38:04 +0100

    Update assignment

--- cachematrix.R
+++ cachematrix.R
@@ -10,14 +10,14 @@ makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                i <<- NULL
         }
 ## get the value of the matrix.
         get <- function() x
                 
 ## set the inverse of the matrix.
-        setInv <- function(i) m <<- i
-        getInv <- function() m
+        setInv <- function(inverse) i <<- inverse
+        getInv <- function() i
 
 ## produce a list
          list(set = set, get = get,
@@ -26,38 +26,45 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 # Example
-# matrixA <- matrix(c(2, 4, 3, 1, 5, 7, 4, 0, 9), # the data elements 
-#             nrow=3,              # number of rows 
-#             ncol=3,              # number of columns 
-#             byrow = TRUE)        # fill matrix by rows 
-# 
-# listB <- makeCacheMatrix(matrixA)
+
+listA <- makeCacheMatrix()
+class(listA)  - shows that listB is a list
+class(listA$set) # shows that the elements of the list are functions
+listA$set(matrix(c(2, 4, 3, 1, 5, 7, 4, 0, 9), 
+                 ncol=3, byrow = TRUE))       #set the matrix
+listA$get()
+
+
 ## Write a short comment describing this function
 
-## Computes the inverse on the matrix within the list
+# cacheSolve: This function computes the inverse of the special "matrix"
+# returned by makeCacheMatrix above. If the inverse has already been calculated
+# (and the matrix has not changed), then the cachesolve should retrieve the
+# inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## get the inverse of the matrix.
-#         x <- listB
-        m <- x$getInv()
+        #       x <- listB
+        i <- x$getInv()
         
         ## check if there is the matrix, if yes: print the message.
-        if(!is.null(m)) {
+        if(!is.null(i)) {
                 print("getting cached data")
-                return(m)
+                return(i)
         }
         ## if not: get the inverse of the matrix.
         data <- x$get()
-        m <- solve(data, ...)
+        i <- solve(data, ...)
         ## set the inverse of the matrix.
-        x$setInv(m)
-        m
+        x$setInv(i)
+        i
         ## Return a matrix that is the inverse of 'x'
 }
 
 # Example
-# cacheSolve(listB)
-# solve(matrixA)                #same result as cacheSolve
+cacheSolve(listA)
+solve(matrix(c(2, 4, 3, 1, 5, 7, 4, 0, 9), 
+             ncol=3, byrow = TRUE))#same result as cacheSolve
 
 
 
